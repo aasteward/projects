3784da4a51fdcb56e3065bdd8d2f25d8a97468c7
Aaron Steward <aa.daly.stew@gmail.com>
2017-02-14 10:51:18 -0600

    Can paint colors onto the mondrian.

--- mondrian/scripts.js
+++ mondrian/scripts.js
@@ -1,14 +1,30 @@
 window.addEventListener("load", function() {
 
-	var easel = document.getElementById("workspace")
-	easel.addEventListener("click", function(e) {
-		console.log(event.target);
-	});
+	// SET BRUSH COLOR TRIGGER
+	var palette = document.getElementById("color_palette")
+	palette.addEventListener("click", brushColor)
 
-	// SET BRUSH COLOR
-	function pickColor(){
-		if (event.target.class = "color") {
-			console.log("You picked the color " + event.target.id)
-		}
-	}
+	// SET BOX COLOR TRIGGER
+	var easel = document.getElementById("painting")
+	easel.addEventListener("click", boxColor)
+
 });
+
+// CHANGE BRUSH COLOR
+var current_color = "white"
+function brushColor(){
+	if (event.target == document.getElementById("red")) {
+		current_color = "#cc0000"
+	} else if (event.target == document.getElementById("yellow")) {
+		current_color = "#ffec00"
+	} else if (event.target == document.getElementById("blue")) {
+		current_color = "#0000cc"
+	} else if (event.target == document.getElementById("white")) {
+		current_color = "white"
+	}
+}
+
+// CHANGE BOX COLOR
+function boxColor(){
+	event.target.style.background = current_color
+}
